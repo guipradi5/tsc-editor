2a0e44cbfca2566b1f71288a4f6a85c332429c4b
Guille <guipradi5@gmail.com>
2025-04-20 17:03:54 +0200

    Cambios para añadir el texto en ingles y poner el id debajo del nombre del PJ

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ import './style.css'
 import { JSONParser } from '@streamparser/json';
 
 let loadedJSON = {}
+let ogLoadedJSON = {}
 let displayJSON = {}
+let ogDisplayJSON = {}
 let downloadJSON = {}
 
 let fileName = ''
@@ -10,107 +12,126 @@ let fileName = ''
 let loadingMessage = document.querySelector('#loading')
 loadingMessage.style.display = 'none'
 
-function uploadJSONFile(e) {
+async function uploadJSONFile(e) {
     loadingMessage.style.display = 'block'
     const input = e.target
     let file = input.files[0];
+    fileName = input.files[0].name
+
+
+
+    const englishJSON = await fetch(`original_texts/${fileName}`)
+
+    let ogFile = await englishJSON.text()
+
+
+
+
+
 
     let reader = new FileReader();
-
     reader.readAsText(file);
 
-    reader.onload = function() {
-        fileName = input.files[0].name
+    reader.onload = function () {
         document.querySelector('#fileName').innerHTML = fileName
-        let keys = []
 
-        loadedJSON = { dic: {}, stringDic: {}}
 
-        const parser = new JSONParser();
-        parser.onValue = ({ key, value }) => {
-            if(value && (value.hasOwnProperty('messageEN') || value.hasOwnProperty('stringEN'))) {
-                const upperKey = value.hasOwnProperty('messageEN') ? 'dic' : 'stringDic'
-                let lowerKey = key
-                if(keys.includes(key)){
-                    const number = keys.filter((savedKey) => savedKey === key).length + 1
-                    lowerKey += `__${number}`
-                }
-                keys.push(key)
-                
-                loadedJSON[upperKey][lowerKey] = value
-            }
-        };
-        
-        // Or passing the stream in several chunks 
-        try {
-            parser.write(reader.result.replaceAll('\\\\', '\\'));
-        } catch (err) {
-            console.log(err); // handler errors 
-            loadingMessage.style.display = 'none'
-        }
+        loadedJSON = parseStuff(reader.result.replaceAll('\\\\', '\\'))
+        ogLoadedJSON = parseStuff(ogFile)
 
         console.log("JSON:")
         console.log(loadedJSON)
+        console.log(ogLoadedJSON)
 
         downloadJSON = JSON.parse(JSON.stringify(loadedJSON))
         displayJSON = JSONtoDisplay(loadedJSON)
-        renderJSON(displayJSON)
-        
+        ogDisplayJSON = JSONtoDisplay(ogLoadedJSON)
+        renderJSON(displayJSON, ogDisplayJSON)
+
         loadingMessage.style.display = 'none'
     };
 
-    reader.onerror = function() {
+    reader.onerror = function () {
         console.log(reader.error);
         loadingMessage.style.display = 'none'
     };
 }
 
+function parseStuff(stuff) {
+    let keys = []
+    const theJSON = { dic: {}, stringDic: {} }
+    const parser = new JSONParser();
+    parser.onValue = ({ key, value }) => {
+        if (value && (value.hasOwnProperty('messageEN') || value.hasOwnProperty('stringEN'))) {
+            const upperKey = value.hasOwnProperty('messageEN') ? 'dic' : 'stringDic'
+            let lowerKey = key
+            if (keys.includes(key)) {
+                const number = keys.filter((savedKey) => savedKey === key).length + 1
+                lowerKey += `__${number}`
+            }
+            keys.push(key)
+
+            theJSON[upperKey][lowerKey] = value
+        }
+    };
+
+    // Or passing the stream in several chunks 
+    try {
+        parser.write(stuff);
+        return theJSON
+    } catch (err) {
+        console.log(err); // handler errors 
+        loadingMessage.style.display = 'none'
+    }
+}
+
 function JSONtoDisplay(data) {
     let dic = {}
     let stringDic = {}
-    for(let key in data.dic){
-      const chrName = data.dic[key].chrName
-      const messageEN = data.dic[key].messageEN
-      const messageJP = data.dic[key].messageJP
-      Object.assign(dic, { [key]: {chrName, messageEN, messageJP} })
+    for (let key in data.dic) {
+        const chrName = data.dic[key].chrName
+        const messageEN = data.dic[key].messageEN
+        const messageJP = data.dic[key].messageJP
+        Object.assign(dic, { [key]: { chrName, messageEN, messageJP } })
     }
-  
-    for(let key in data.stringDic){
+
+    for (let key in data.stringDic) {
         const stringEN = data.stringDic[key].stringEN
         const stringJP = data.stringDic[key].stringJP
-      Object.assign(stringDic, { [key]: {stringEN, stringJP} })
+        Object.assign(stringDic, { [key]: { stringEN, stringJP } })
     }
-  
-    return {...data, dic, stringDic}
+
+    return { ...data, dic, stringDic }
 }
 
-function renderJSON(json, filter){
+function renderJSON(json, ogJson, filter) {
     const displayWrapper = document.querySelector('#display-wrapper')
     displayWrapper.innerHTML = `
     <div class="line header">
-        <div>ID</div>
-        <div style='text-align:right;'>Personaje</div>
+        <div style='text-align:right;'>ID y Personaje</div>
         <div style='min-width: calc(10px* 35);'>Español</div>
+        <div style='min-width: calc(10px* 35);'>Inglés</div>
         <div style='min-width: calc(10px* 35);'>Original</div>
     </div>`
 
     let finalHTML = ''
     const totalKeys = Object.keys(json.dic).length + Object.keys(json.stringDic).length
     let keyCounter = 0
-    for(const id in json.dic){
+    for (const id in json.dic) {
         keyCounter++
         updatePercent(totalKeys, keyCounter, 0.98)
         finalHTML += `
             <div class='line dic'>
-                <div class='id'>
-                    ${id}
-                </div>
                 <div class='char'>
                     <div><img src='portraits/FACE_${json.dic[id].chrName}.png' onerror="this.style.display='none'" /></div>
                     <div>${json.dic[id].chrName}</div>
+                    <div class="lineId"> ${id} </div>
                 </div>
                 <div class="text-area-wrapper">
                     <textarea class="ES-text" id='${id}' data-type='dic' wrap='off'>${json.dic[id].messageEN.replaceAll('\\n', '\n')}</textarea>
+                </div>
+                <div class="text-area-wrapper">
+                    <textarea class="ES-text" id='EN_${id}' disabled data-type='dic' wrap='off'>${ogJson.dic[id] ? ogJson.dic[id].messageEN.replaceAll('\\n', '\n') : 'NOT FOUND'}</textarea>
                 </div>
                 <textarea disabled wrap='off'>${json.dic[id].messageJP.replaceAll('\\n', '\n')}</textarea>
             </div>
@@ -118,7 +139,7 @@ function renderJSON(json, filter){
     }
 
     finalHTML += '<h4 class="mensajes">Mensajes</h4>'
-    for(const id in json.stringDic){
+    for (const id in json.stringDic) {
         keyCounter++
         updatePercent(totalKeys, keyCounter, 0.98)
         finalHTML += `
@@ -126,26 +147,28 @@ function renderJSON(json, filter){
                 <div class='id'>
                     ${id}
                 </div>
-                <div></div>
                 <div class="text-area-wrapper">
                     <textarea class="ES-text" id='${id}' data-type='stringDic' class='${json.stringDic[id].stringEN.length > 30 ? 'warning' : ''}' wrap='off'>${json.stringDic[id].stringEN.replaceAll('\\n', '\n')}</textarea>
+                </div>
+                <div class="text-area-wrapper">
+                    <textarea class="ES-text" id='${id}' data-type='stringDic' class='${ogJson.stringDic[id].stringEN.length > 30 ? 'warning' : ''}' wrap='off'>${json.stringDic[id].stringEN.replaceAll('\\n', '\n')}</textarea>
                 </div>
                 <textarea disabled wrap='off'>${json.stringDic[id].stringJP.replaceAll('\\n', '\n')}</textarea>
             </div>
         `
     }
     displayWrapper.innerHTML += finalHTML
-    
+
     const allTextAreas = document.querySelectorAll('.ES-text')
-    for(const textarea of allTextAreas) {
+    for (const textarea of allTextAreas) {
         textarea.style.height = "15px";
         textarea.style.height = (textarea.scrollHeight + 15) + "px";
 
         textarea.addEventListener('input', (e) => changeText(e, textarea.dataset.type))
 
     }
-    
-    if(chiaro){
+
+    if (chiaro) {
         document.querySelector('#display-wrapper').classList.add('chiaro')
     } else {
         document.querySelector('#display-wrapper').classList.remove('chiaro')
@@ -156,14 +179,14 @@ function renderJSON(json, filter){
 function changeText(e, prop) {
     const newText = e.target.value.replaceAll('\n', '\\n')
     const id = e.target.id
-    if(prop === 'dic')
+    if (prop === 'dic')
         downloadJSON[prop][id].messageEN = newText
     else
         downloadJSON[prop][id].stringEN = newText
     e.target.style.height = "15px";
     e.target.style.height = (e.target.scrollHeight + 15) + "px";
-    if(prop === 'stringDic') {
-        if(newText.length > 30){
+    if (prop === 'stringDic') {
+        if (newText.length > 30) {
             e.target.classList.add('warning')
         } else {
             e.target.classList.remove('warning')
@@ -174,7 +197,7 @@ function changeText(e, prop) {
 function downloadTheJSON() {
     var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(downloadJSON).replaceAll('\\', '\\\\').replace(/\_\_\d+/g, ''));
     var downloadAnchorNode = document.createElement('a');
-    downloadAnchorNode.setAttribute("href",     dataStr);
+    downloadAnchorNode.setAttribute("href", dataStr);
     downloadAnchorNode.setAttribute("download", fileName);
     document.body.appendChild(downloadAnchorNode); // required for firefox
     downloadAnchorNode.click();
@@ -182,10 +205,10 @@ function downloadTheJSON() {
 }
 
 function reemplazar(term1, term2) {
-    
+
 }
 
-function updatePercent(total, partial, factor = 1){
+function updatePercent(total, partial, factor = 1) {
     const finalPercent = ((partial * 100) / total) * factor
     document.querySelector('#percent').innerHTML = finalPercent
 }
@@ -207,4 +230,3 @@ function changeFont() {
 }
 
 document.querySelector('#font').addEventListener('click', changeFont)
-
